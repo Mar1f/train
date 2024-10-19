6704ee081d0acc9f53b552c17734640c3feac4e3
Mar1f <938224402@qq.com>
2024-10-19 17:11:32 +0800

    力扣80

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,12 @@
  **/
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1, 1, 2}; // 使用大括号初始化数组
+        int[] nums = {0,0,1,1,1,1,2,3,3}; // 使用大括号初始化数组
         Solution solution = new Solution(); // 创建Solution类的实例
         int k = solution.removeDuplicates(nums); // 调用方法
         System.out.println("新长度: " + k); // 输出结果
+        for(int i=0;i<nums.length;i++)
+        System.out.println(nums[i]);
     }
 }
 // 1 1 2
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,31 +5,40 @@
  **/
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int fast = 1;
-        int slow =0;
-        if(nums.length == 0){
-            return 0;
+        int i = 0;
+        for (int x : nums) {
+            if (i < 2 || nums[i- 2] != x) nums[i++] = x;
         }
-        else{
-            while(fast<nums.length){
-//                System.out.println("slow:"+slow+"  nums[slow]:"+nums[slow]);
-//                System.out.println("fast:"+fast+"  nums[fast]:"+nums[fast]);
-
-                if(nums[fast]==nums[slow]){
-                    fast++;
-                }
-                else{
-//                    System.out.println("slow:"+slow+"  nums[slow]:"+nums[slow]);
-//                    System.out.println("fast:"+fast+"  nums[fast]:"+nums[fast]);
-                    slow++;
-                    System.out.println(slow);
-                    nums[slow]=nums[fast];
-                    System.out.println(nums[slow]);
-                    System.out.println(slow);
-                    fast++;
-                }
-            }
-            return slow;
-        }
+        return i;
     }
 }
+//class Solution {
+//    public int removeDuplicates(int[] nums) {
+//        int fast = 1;
+//        int slow =0;
+//        if(nums.length == 0){
+//            return 0;
+//        }
+//        else{
+//            while(fast<nums.length){
+////                System.out.println("slow:"+slow+"  nums[slow]:"+nums[slow]);
+////                System.out.println("fast:"+fast+"  nums[fast]:"+nums[fast]);
+//
+//                if(nums[fast]==nums[slow]){
+//                    fast++;
+//                }
+//                else{
+////                    System.out.println("slow:"+slow+"  nums[slow]:"+nums[slow]);
+////                    System.out.println("fast:"+fast+"  nums[fast]:"+nums[fast]);
+//                    slow++;
+//                    System.out.println(slow);
+//                    nums[slow]=nums[fast];
+//                    System.out.println(nums[slow]);
+//                    System.out.println(slow);
+//                    fast++;
+//                }
+//            }
+//            return slow;
+//        }
+//    }
+//}
