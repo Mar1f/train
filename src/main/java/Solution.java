0d9615b382da6b9c31612a3ee5d0d5efb6052dc1
Mar1f <938224402@qq.com>
2024-10-20 18:26:23 +0800

    力扣122

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -7,18 +7,22 @@ import static java.lang.Math.max;
  * @author:mar1
  * @data:2024/10/19
  **/
-public class Solution {
-    public int maxProfit(int prices[]) {
-        int minprice = Integer.MAX_VALUE;
-        int maxprofit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < minprice) {
-                minprice = prices[i];
-            } else if (prices[i] - minprice > maxprofit) {
-                maxprofit = prices[i] - minprice;
-            }
+//、
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n < 2) {
+            return 0;
         }
-        return maxprofit;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i=1;i<n;i++){
+            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]+prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0]-prices[i]);
+        }
+        return dp[n-1][0];
+
     }
 }
 
